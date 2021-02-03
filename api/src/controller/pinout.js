f96648333394f338e2b78d53000c9dcbbab7d4f6
Tikin - Luiz Carlos <d71k1n@live.com>
2021-02-03 14:44:32 -0300

    docs: criado comentario para documentação.

--- api/src/controller/pinout.js
+++ api/src/controller/pinout.js
@@ -1,11 +1,14 @@
 var Gpio = require('onoff').Gpio;
 
+/**
+ * Rotas de PINOUT
+ */
 module.exports.autoroute = {
-get:{  
-  "/pinout/:pin": status,
-  "/pinout/": status,
+  get: {
+    "/pinout/:pin": status,
+    "/pinout/": status,
 
-},
+  },
   put: {
     "/pinout/mode/:pin/:mode": mode,
   },
@@ -14,39 +17,59 @@ get:{
   }
 };
 
-function mode(req, res) {
-  const io = req.params.mode == "i"?"in":"out";
-  const pin = req.params.pin;
-  
-  global.RASPBERRY[pin] = new Gpio(pin,io);  
-  
-  res.json({
-    "pin":pin,
-    "mode":io,
-    "variavel global":global.RASPBERRY[2]
 
+/**
+ * Mode 
+ * 
+ * Define o modo de operação do pino em questão
+ *  
+ * @param {Object} req Requsição recebida
+ * @param {Object} res Resposta para requisição
+ */
+function mode(req, res) {
+  const io = req.params.mode == "i" ? "in" : "out";
+  const pin = req.params.pin;
+
+  global.RASPBERRY[pin] = new Gpio(pin, io);
+
+  res.json({
+    "pin": pin,
+    "mode": io
   })
 }
 
+/**
+ * Set
+ * 
+ * Seta o status do pino em questão. 0 ou 1 (ativo ou inativo) 
+ * 
+ * @param {Object} req Requsição recebida
+ * @param {Object} res Resposta para requisição
+ */
 function set(req, res) {
   const pin = req.params.pin;
   const status = req.params.status;
- 
-  global.RASPBERRY[pin].writeSync(parseInt(status));      
+
+  global.RASPBERRY[pin].writeSync(parseInt(status));
 
   res.json({
-    "pin":pin,
-    "state": status,
-    "variavel global":global.RASPBERRY[pin]
+    "pin": pin,
+    "state": status
   })
 }
 
-function status(req,res) {
+/**
+* Status
+* Retorna o status do pino em questão.
+* @param {Object} req Requsição recebida
+* @param {Object} res Resposta para requisição
+*/
+function status(req, res) {
   const pin = req.params.pin
   if (pin) {
-    res.json({pin,"status":2})
-  }else{
+    res.json({ pin, "status": 2 })
+  } else {
     res.json("todos os pinos")
   }
-  
+
 }
